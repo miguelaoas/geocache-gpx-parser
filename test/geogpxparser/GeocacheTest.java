8c7b886f84ad106b432f0d69211f42b9c46976ac
villes01@gmail.com <villes01@gmail.com@c2e0f2df-efb4-8810-1372-eed99ad4b568>
2012-09-11 16:11:17 +0000

    Added some comments.

--- test/geogpxparser/GeocacheTest.java
+++ test/geogpxparser/GeocacheTest.java
@@ -1,35 +1,41 @@
 package geogpxparser;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * This is the unit test class for the Geocache class.
- * @author ZeroOne
+ *
+ * @author Ville Saalo (http://coord.info/PR32K8V)
  */
 public class GeocacheTest {
 
     @Test
     public void testHint() {
-        System.out.println("hint");
+        System.out.println("testHint");
+        
+        // Test the basic case of getting the hint as encrypted:
         boolean decrypted = false;
         Geocache instance = new Geocache();
         instance.setHint("kiven alla");
         String expResult = "xvira nyyn";
         String result = instance.getHint(decrypted);
         assertEquals(expResult, result);
-        
+
+        // Test the basic case of getting the hint as decrypted:
         expResult = "kiven alla";
         decrypted = true;
         result = instance.getHint(decrypted);
         assertEquals(expResult, result);
-        
+
+        // Test getting the encrypted hint with some non-alphabetic characters:
         instance.setHint("@[`{ KIVEN YLLÄ 123");
         expResult = "@[`{ XVIRA LYYÄ 123";
         decrypted = false;
         result = instance.getHint(decrypted);
         assertEquals(expResult, result);
-        
+
+        // Test getting the decrypted hint with some non-alphabetic characters:
         expResult = "@[`{ KIVEN YLLÄ 123";
         decrypted = true;
         result = instance.getHint(decrypted);
